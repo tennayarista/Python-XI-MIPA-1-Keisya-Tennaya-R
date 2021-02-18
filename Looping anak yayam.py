da40f1aae80bf176b2dfb2110a29625b31919d9f
tennayarista <tennayarista@gmail.com>
2021-02-18 08:03:48 +0700

    lain-lain

---
+++ Looping anak yayam.py
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<cstdlib>
+using namespace std;
+
+int main()
+{
+    int Ayam;
+
+    cout<<"Anak Ayam:";
+    cin>>Ayam;
+    for(Ayam; Ayam>1; Ayam--)
+    {
+        cout<<"Anak Ayam turunlah "<<Ayam<<"Mati 1 tinggal "<<Ayam-1<<""<<end1;
+    }
+    cout<<"anak Ayam turunlah 1 Mati 1 tinggal induknya"<<end1;
+    system("pause");
+    return 0;
