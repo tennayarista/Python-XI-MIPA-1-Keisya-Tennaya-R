aab0ce1d31c63248b8eaea6d48e66c5867aa860c
tennayarista <tennayarista@gmail.com>
2021-02-18 08:13:07 +0700

    Latihan Pemrograman

---
+++ Nomor 2 swapList.py
@@ -0,0 +1,11 @@
+def swaplist(newList):
+    size = len(newList)
+
+    temp = newList[0]
+    newList[0] = newList[size - 1]
+    newList[size - 1] = temp
+
+    return newList
+
+newList = [12,35,9,56,24]
+print(swaplist(newList))
